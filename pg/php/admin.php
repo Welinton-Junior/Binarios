21665ffc4d31f78f11acf6fbc1adf2e99e0f95fa
Welinton-Junior <126504291+Welinton-Junior@users.noreply.github.com>
2023-10-25 22:43:20 -0300

    Add files via upload

--- pg/php/admin.php
+++ pg/php/admin.php
@@ -74,47 +74,57 @@ if (!$resultado) {
         </div>
     </div>
     </div>
-    <div class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4 col-sm-4 col-xs-12">
-                        <div class="single_footer">
-                            <h4>Services</h4>
-                            <ul>
-                                <li><a href="./pg/html/adminLogin.html">Lorem Ipsum</a></li>
-                                <li><a href="#">Simply dummy text</a></li>
-                  
-                            </ul>
-                        </div>
-                    </div>
-                    <!--- END COL -->
-                    <div class="col-md-4 col-sm-4 col-xs-12">
-                        <div class="single_footer single_footer_address">
-                            <h4>Midias Sociais</h4>
-                            <ul>
-                                <li><a href="https://www.facebook.com/binarios">FaceBook</a></li>
-                                <li><a href="https://www.instagram.com/binarios/">Instagram</a></li>
-                              
-                            </ul>
-                        </div>
-                    </div>
-                    <!--- END COL -->
-                    <div class="col-md-4 col-sm-4 col-xs-12">
+    <div class="footer bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-sm-4 col-xs-12">
+                    <div class="single_footer bg-">
+                        <a href="../../index.html"><img src="../../img/logo-branco.png" width="250" srcset=""></a>
 
                     </div>
-                    <!--- END COL -->
                 </div>
-                <!--- END ROW -->
-                <div class="row">
-                    <div class="col-lg-12 col-sm-12 col-xs-12">
-                        <p class="copyright">Copyright © 2023 <a href="#">BINAR10S</a>.</p>
+
+
+                <div class="col-lg-4 col-sm-4 col-xs-12">
+                    <div class="single_footer">
+                        <h4>Contato</h4>
+                        <ul>
+                            <li><a href="../html/contato.html">Iniciar conversa</a></li>
+
+
+                        </ul>
                     </div>
-                    <!--- END COL -->
                 </div>
-                <!--- END ROW -->
+
+
+                <!--- END COL -->
+                <div class="col-md-4 col-sm-4 col-xs-12">
+                    <div class="single_footer single_footer_address">
+                        <h4>Midias Sociais</h4>
+                        <ul>
+                            <li><a href="https://www.facebook.com/binarios">Facebook</a></li>
+                            <li><a href="https://www.instagram.com/binarios/">Instagram</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+                <!--- END COL -->
+                <div class="col-md-4 col-sm-4 col-xs-12">
+
+                </div>
+                <!--- END COL -->
             </div>
-            <!--- END CONTAINER -->
+            <!--- END ROW -->
+            <div class="row">
+                <div class="col-lg-12 col-sm-12 col-xs-12">
+                    <p class="copyright">Copyright © 2023 <a href="#">BINAR10S</a>.</p>
+                </div>
+                <!--- END COL -->
+            </div>
+            <!--- END ROW -->
         </div>
+        <!--- END CONTAINER -->
+    </div>
 
 </body>
 
